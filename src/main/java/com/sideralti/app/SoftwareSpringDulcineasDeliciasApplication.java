13f3fd313c0cca3a22baba3272c55d4200d53593
Sideral TI <sideralti.app@gmail.com>
2025-02-01 21:56:45 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringDulcineasDeliciasApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringDulcineasDeliciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringDulcineasDeliciasApplication.class, args);
+	}
+
+}
